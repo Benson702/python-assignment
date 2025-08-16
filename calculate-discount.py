111d49ed5975fb8faba013b31da797cc01b84380
Benson Kamau <bensokamau611@gmail.com>
2025-08-16 17:54:29 +0300

    wk 3 assignment

--- calculate-discount.py
+++ calculate-discount.py
@@ -3,11 +3,17 @@ def calculate_discount(price, discount_percent):
     The fun should take the original price and the discount percentage as parameters.
     If the discount is 20% or higher apply discount otherwise return the original price.
     """
-    if discount_percent >= 20:
+    if price < 0 or discount_percent >= 20:
         discount_amount=price * discount_percent/100
         final_price=price - discount_amount
         return final_price
     else:
         return price
-print(calculate_discount(200, 25))  
-print(calculate_discount(200, 10))  
+original_price = float(input("Enter the original_price"))
+discount_percent = float(input("Enter the discountt_percent"))
+
+final_price = calculate_discount(original_price, discount_percent)
+if discount_percent >= 20:
+    print(f"Final price applying a {discount_percent}% discount: ${final_price}")
+else:
+    print(f"No discount applied. original price: ${original_price:.2f}")
